9292262a6a096ff1d3b003e52aaf86053d4b77d8
Matteo <matteop18@gmail.com>
2019-12-16 15:48:29 +0100

    inserito js

---
+++ js/javascript.js
@@ -0,0 +1,3 @@
+document.getElementById("title_song").innerHTML = "Bastille";
+document.getElementById("top_ten").innerHTML = "Top 10";
+document.getElementById("top_italy").innerHTML = "Top 100 Italy";
